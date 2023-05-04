a44cf724314b111e9e2357e20ce08ce43a950862
AdrienVers <adrienvx77@outlook.fr>
2023-05-04 10:27:27 +0200

    test structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,13 @@ import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { TeamComponent } from "./team/team.component";
 import { AnalyseComponent } from "./analyse/analyse.component";
-import { NgChartsModule } from "ng2-charts";
 import { OverviewComponent } from "./home/components/overview/overview.component";
 import { HomeFinancesComponent } from "./home/components/home-finances/home-finances.component";
 import { HomeProjectsComponent } from "./home/components/home-projects/home-projects.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { PresentationComponent } from "./home/components/overview/components/presentation/presentation.component";
 import { StatisticsComponent } from "./home/components/overview/components/statistics/statistics.component";
+import { NgChartsModule } from "ng2-charts";
 
 @NgModule({
 	declarations: [
